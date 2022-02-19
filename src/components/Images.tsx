9520a57e7c1655c6b211472adf8058114bbe6596
Ismail Firman <79613677+elfirman@users.noreply.github.com>
2022-02-19 14:09:10 +0700

    Add files via upload

---
+++ src/components/Images.tsx
@@ -0,0 +1,11 @@
+import Image_Logo from "@public/images/logo.svg";
+export const Logo = Image_Logo;
+
+import Image_MountainLandscape from "@public/images/illustrations/mountain-landscape.svg";
+export const MountainLandscape = Image_MountainLandscape;
+
+import Image_SeaFloor from "@public/images/illustrations/sea-floor.svg";
+export const SeaFloor = Image_SeaFloor;
+
+import Image_Rocket from "@public/images/illustrations/rocket.svg";
+export const Rocket = Image_Rocket;
