cd1859ca1bb1c682e5206f9a32814801fac1d60d
Ismail Firman <79613677+elfirman@users.noreply.github.com>
2022-04-30 01:28:58 +0700

    Delete src directory

--- src/pages/404.tsx
+++
@@ -1,5 +0,0 @@
-import ErrorPage from "@/components/ErrorPage";
-
-export default function Error404Page() {
-    return <ErrorPage statusCode={404} message={"It seems that you've been lost."} />;
-}
--- src/pages/500.tsx
+++
@@ -1,5 +0,0 @@
-import ErrorPage from "@/components/ErrorPage";
-
-export default function Error404Page() {
-    return <ErrorPage statusCode={500} message={"Server-side error occurred."} />;
-}
